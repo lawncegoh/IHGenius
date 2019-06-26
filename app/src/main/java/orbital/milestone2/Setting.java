452fefae2579cb4d3f12dd3aeba79832974be41f
Lawnce Goh <lawnce@gmail.com>
2019-06-26 17:43:30 +0800

    26 June

--- app/src/main/java/orbital/milestone2/Setting.java
+++ app/src/main/java/orbital/milestone2/Setting.java
@@ -2,18 +2,17 @@ package orbital.milestone2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class Homepage extends AppCompatActivity {
+public class Setting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+        setContentView(R.layout.activity_setting);
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,17 +20,17 @@ public class Homepage extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.navigation_home:
-                        Intent intent = new Intent(Homepage.this, Homepage.class);
+                        Intent intent = new Intent(Setting.this, Homepage.class);
                         startActivity(intent);
                         break;
 
                     case R.id.navigation_information:
-                        Intent intent1 = new Intent(Homepage.this, Information.class);
+                        Intent intent1 = new Intent(Setting.this, Information.class);
                         startActivity(intent1);
                         break;
 
                     case R.id.navigation_settings:
-                        Intent intent2 = new Intent(Homepage.this, Setting.class);
+                        Intent intent2 = new Intent(Setting.this, Setting.class);
                         startActivity(intent2);
                         break;
                 }
